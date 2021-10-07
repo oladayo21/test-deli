38e71aa3775183bb6e97cca922cfd60431a0bb0b
Oladayo Fagbemi <oladayo.fagbemi@axelspringer.com>
2021-10-07 11:25:34 +0200

    edited workflow again

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+dir_name=./public
+
+function countDirectories(){
+    echo $(find ${dir_name} -name 'test-*' -type d | wc -l)
+    
+}
+function test(){
+    dir_count=$(countDirectories)
+[[ dir_count -gt 0 ]] && echo "Test page built" && exit 1 || echo "No test Pages built" && exit 0
+}
+test
