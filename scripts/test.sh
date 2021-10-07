88c6a1c6d6c09f56d26f77bfca5c5e349edea859
Oladayo Fagbemi <oladayo.fagbemi@axelspringer.com>
2021-10-07 11:47:48 +0200

    Testing new fn

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,15 @@
 #!/bin/zsh
 dir_name=./public
-
+function exitWithError() {
+  echo "Error: $1"
+  exit 1
+}
 function countDirectories(){
-    echo $(find ${dir_name} -name 'test-*' -type d | wc -l)
+    echo $(find ${dir_name} -name 'test*' -type d | wc -l)
     
 }
 function test(){
     dir_count=$(countDirectories)
-[[ dir_count -gt 0 ]] && echo "Test page built" && exit 1 || echo "No test Pages built" && exit 0
+[[ dir_count -gt 0 ]] && exitWithError "Test page should not be built" ||  exit 0
 }
 test
